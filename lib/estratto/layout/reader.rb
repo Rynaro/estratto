e16e2137214e657b4b645aa5729425adbc299bfe
Inácio de Farias <inacio.avf@gmail.com>
2020-10-30 18:12:02 -0300

    Change Estratto::Layout::Reader to work with Hashes as the layout: argument

--- lib/estratto/layout/reader.rb
+++ lib/estratto/layout/reader.rb
@@ -1,16 +1,24 @@
 require 'yaml'
 
+require_relative '../helpers/hash'
+
 module Estratto
   module Layout
     class Reader
-      attr_reader :file
+      attr_reader :template_source
 
-      def initialize(file)
-        @file = file
+      def initialize(template_source)
+        @template_source = template_source
       end
 
       def template
-        @template ||= YAML.load_file(file)
+        @template ||= begin
+          if template_source.is_a?(Hash)
+            Estratto::Helpers::Hash.deep_transform_keys(template_source, &:to_s)
+          elsif template_source.is_a?(String)
+            YAML.load_file(template_source)
+          end
+        end
       end
 
       def layout
@@ -24,7 +32,6 @@ module Estratto
       def multiregister?
         layout.dig('multi-register') || false
       end
-
     end
   end
 end
